eb2e846b79b9c4719e8b7d0610784b7ab04a13aa
覃福超 <qinfuchao@qiyi.com>
2019-12-18 10:15:19 +0800

    First Blood

---
+++ app/src/main/java/com/chumfuchiu/code/quitelint/MainActivity.java
@@ -0,0 +1,14 @@
+package com.chumfuchiu.code.quitelint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
